87e4961a783c08bb47bed731983c60a86ef196a6
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-18 08:07:36 +0300

    Silly neon effect
    Seems even sillier...

--- public/scripts/ukuli.js
+++ public/scripts/ukuli.js
@@ -1,16 +1,17 @@
 jQuery( document ).ready(function() {
     setInterval(function(){
         var onOrOff = Math.floor((Math.random() * 2));
-        setNeonLights();
+        console.log(onOrOff);
+        setNeonLights(onOrOff);
     }, 3000);
 });
 
 function setNeonLights(onOrOff) {
     if(onOrOff) {
-        jQuery(".neonlights-off").addClass(".neonlights-on");
-        jQuery(".neonlights-off").removeClass(".neonlights-off");
+        jQuery(".neonlights-off").addClass("neonlights-on");
+        jQuery(".neonlights-off").removeClass("neonlights-off");
     } else {
-        jQuery(".neonlights-on").addClass(".neonlights-off");
-        jQuery(".neonlights-on").removeClass(".neonlights-on");
+        jQuery(".neonlights-on").addClass("neonlights-off");
+        jQuery(".neonlights-on").removeClass("neonlights-on");
     }
 }
